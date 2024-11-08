fd81d907b1696c68d45b496e61cf94ec698642d0
Quinn Voboril <58952363+QVBRL@users.noreply.github.com>
2024-11-08 14:31:38 -0700

    LOMBOK

--- Manager/src/main/java/Durak/Game/Impl/GameState.java
+++ Manager/src/main/java/Durak/Game/Impl/GameState.java
@@ -3,7 +3,10 @@ package Durak.Game.Impl;
 import java.util.List;
 
 import Durak.Player.impl.Player;
+import lombok.Builder;
 
+
+@Builder
 public class GameState {
  
     public List<Player> players;
